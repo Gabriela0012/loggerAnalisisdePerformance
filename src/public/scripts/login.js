9744e8609379f1c7a64b4899438a5034557fcc25
gabriela Arenas <gabega10@gmail.com>
2022-10-23 19:57:14 -0300

    href

---
+++ src/public/scripts/login.js
@@ -0,0 +1,23 @@
+
+const ingresar = document.getElementById('ingresar')
+const  registre = document.getElementById('registre')
+
+registre.addEventListener('click', (e) => {
+  e.preventDefault()
+
+	
+	
+
+	{{config.app.DOMAIN}}
+
+})
+
+ingresar.addEventListener('click', (e) => {
+  e.preventDefault()
+
+	
+	
+	location.href = 'welcome'
+
+
+})
