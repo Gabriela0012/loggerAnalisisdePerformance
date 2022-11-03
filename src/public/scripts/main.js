786b144d81b77c9f5af73ea70f7113bd10ad062e
gabriela Arenas <gabega10@gmail.com>
2022-11-03 20:51:46 -0300

    url

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -1,4 +1,4 @@
-const serverUrl ='http://localhost:8080/'
+const serverUrl ='https://loggeranalisisdeperformance-production.up.railway.app/'
 const itemsPath ='items/'
 const thumbnailPath = 'img/'
 
