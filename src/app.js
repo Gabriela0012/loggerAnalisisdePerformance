300006c51bdfdcf4251136f49c8bc2c1a74bbb32
gabriela Arenas <gabega10@gmail.com>
2022-11-03 18:45:52 -0300

    cambio

--- src/app.js
+++ src/app.js
@@ -62,39 +62,3 @@ app.use('/api/message',messageNormalizerRouter);
 app.use('/api/sessions',sessionsRouter);
 app.use('/api/current',currentRouter);
 
-
-
-const io = new Server(server)
-let products
-let log
-
-io.on('connection', async (socket) => {
-
-  products = await productService.getAllProduct()
-
-  log = await chatService.getAll()
-
-
-
-  console.log('Socket connected')
-  socket.broadcast.emit('newUserConnected')
-  io.emit('log', log)
-  socket.emit('productList', { products })
-
-  socket.on('message', async(data) => {
-    let currentTime = new Date();
-    data.date = currentTime.toLocaleTimeString();
-    await chatService.addChat(data)
-
-    log = await chatService.getAll()
-    io.emit('log', log)
-  })
-    
-  socket.on('addProduct', async (data) => {
-    await productService.addNewProduct(data)
-    products = await productService.getAllProduct()
-    io.emit('productList', { products })
-  })
-
-    
-})
