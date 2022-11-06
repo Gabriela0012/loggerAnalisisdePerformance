3a795547c798e92acef6383c5dfdff653ba2e1a0
gabriela Arenas <gabega10@gmail.com>
2022-11-06 07:44:21 -0300

    sawl

--- src/public/scripts/order.js
+++ src/public/scripts/order.js
@@ -28,16 +28,8 @@ function saveOrderInDatabase(){
 
   updateShoppingCartTotal();
   modal.classList.remove('modal--show')
-  Swal.fire({
-    title: 'Muchas gracias! Se le enviará un mail con el número del pedido',
-    showClass: {
-      popup: 'animate__animated animate__fadeInDown'
-    },
-    hideClass: {
-      popup: 'animate__animated animate__fadeOutUp'
-    }
-  })
 
+ 
   
 
 }
